44b16fe6570590fa2b32b53d674e1a0e0d9f73ea
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2022-01-29 02:03:18 +0530

    account verification alert added

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
 import AdminNavbar from "./Admin/AdminNavbar";
+import AccountVerificationAlertWarning from "./Alerts/AccountVerificationAlertWarning";
 import PrivateNavbar from "./Private/PrivateNavbar";
 import PublicNavbar from "./Public/PublicNavbar";
 
 const Navbar = () => {
   //get user from store
   const state = useSelector((state) => state.users);
-  const {userAuth} = state;
+  const {userAuth,profile} = state;
   const isAdmin = userAuth?.isAdmin;
+  const isAccountVerified = profile?.isAccountVerified;
+
   return (
     <>
       {isAdmin ?( <AdminNavbar isLogin={userAuth}/>):userAuth ?( <PrivateNavbar isLogin={userAuth}/>):(<PublicNavbar/>)}
+
+      {/* Dispaly verification alert */}
+      { !isAccountVerified ? (<AccountVerificationAlertWarning/>):null}
     </>
   );
 };
