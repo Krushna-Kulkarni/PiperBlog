5277e8f305c4ff047c3a3c3f571d7a5d3ac5f611
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2022-01-29 11:03:34 +0530

    redirecting to user profile through postdetails

--- src/components/Posts/PostsList.js
+++ src/components/Posts/PostsList.js
@@ -78,7 +78,7 @@ export default function PostsList() {
               <div className="w-full lg:w-3/4 px-3">
                 {/* Post goes here */}
 
-                {appErr || serverErr ? (<h1>{serverErr} {appErr}</h1>): postLists?.length <=0 ? (<h1 className="text-yellow-400 text-lg text-center">No Post Found</h1>):postLists?.map((post) => ( <div key={post?.id} className="flex flex-wrap bg-gray-900 -mx-3  lg:mb-6">
+                {appErr || serverErr ? (<h1 className="text-red-400 text-center text-lg">{serverErr} {appErr}</h1>): postLists?.length <=0 ? (<h1 className="text-yellow-400 text-lg text-center">No Post Found</h1>):postLists?.map((post) => ( <div key={post?.id} className="flex flex-wrap bg-gray-900 -mx-3  lg:mb-6">
                   <div className="mb-10  w-full lg:w-1/4">
                     < Link to="#">
                       {/* Post image */}
@@ -141,7 +141,7 @@ export default function PostsList() {
                       </div>
                       <div className="ml-3">
                         <p className="text-sm font-medium text-gray-900">
-                          < Link to ="#" className="text-yellow-400 hover:underline ">
+                          < Link to ={`/profile/${post?.user?._id}`} className="text-yellow-400 hover:underline ">
                             {post?.user?.firstName} {post?.user?.lastName}
                           </ Link>
                         </p>
